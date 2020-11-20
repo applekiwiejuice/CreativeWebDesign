e5a2d57a7ee06133f4bcafe2bec9af5a395ce984
applekiwiejuice <mcnikkopinlacnetflix@gmail.com>
2020-11-20 13:47:50 +0800

    added temporary contents, added contact page and team page, fixed mens and more

--- js/forNav.js
+++ js/forNav.js
@@ -1,7 +1,7 @@
 $(window).scroll(function () {
   if ($(document).scrollTop() > 50) {
     $(".nav1").addClass("affix");
-    // console.log("OK");
+    console.log("OK");
   } else {
     $(".nav1").removeClass("affix");
   }
@@ -12,4 +12,12 @@ $(".navTrigger").click(function () {
   console.log("Clicked menu");
   $("#mainListDiv").toggleClass("show_list");
   $("#mainListDiv").fadeIn();
+});
+
+// MODAL POP UP
+// window.jQuery ||
+document.write('<script src="./bootstrap_starter/bootstrap.min.js"></script>');
+
+$(window).on("load", function () {
+  $("#myModal").modal("show");
 });
